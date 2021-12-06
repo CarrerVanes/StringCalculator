d94d4465b7b70ec5a13736836115e618ce1fb210
Vanes <carrervanes@gmail.com>
2021-12-06 11:46:41 +0100

    feat(StringCalc): step 4 (different delimiters), test

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,15 @@
+const digit = /\d/
+const firstline_regex = new RegExp("//.")
+const bad_words = ['\\', '*', '.', '+', '\/', '?', '\|']
+const zero = 0
+
 function add(string) {
-    var res = 0;
-    var delimiter = /,|\n/
+    var res = zero;
+    var delimiter
     if (!!string) { // 1+ numbers
+        var firstline = string.split('\n')[0];
+        var custom_delimiter = getCustomdelimiter(firstline) //get the custom delimiter or ',' if no custom delimiter
+        delimiter = new RegExp("\n|" + custom_delimiter) //build the regex
         var array = string.split(delimiter)
         return sum(array)
     }
@@ -9,11 +17,33 @@ function add(string) {
 }
 
 function sum(array) {
-    var sum = 0
+    var sum = zero
     array.forEach(element => {
-        sum = sum + (isNaN(parseInt(element)) ? 0 : parseInt(element));
+        sum = sum + (isNaN(parseInt(element)) ? zero : parseInt(element)); //if "" or not parsable element -> 0
     })
     return sum;
 }
+
+
+function getCustomdelimiter(firstline) {
+    var res = "," //default delimter
+    if (!digit.test(firstline.charAt(0)) && firstline_regex.test(firstline)) { //not digit and start with /
+        res = firstline.substring(2)
+    }
+    return fixdelimiter(res)
+}
+
+function fixdelimiter(string) {
+    var res = ""
+    for (var i = 0; i < string.length; i++) {
+        if (bad_words.includes(string[i])) { //there is a string
+            res = res + "\\" + string[i]
+        } else {
+            res = res + string[i]
+        }
+    }
+    return res;
+}
+
 
 module.exports = add;
--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,37 +1,94 @@
 const add = require('../src/StringCalculator');
 
-test('\"\"-> 0', () => {
+
+//null string
+test('no numbers: \"\"-> 0', () => {
     expect(add("")).toBe(0);
 });
 
-test('\"1\"-> 1', () => {
+//one number
+test('one number: \"1\"-> 1', () => {
     expect(add("1")).toBe(1);
 });
 
-test('\"1,2\"-> 3', () => {
+test('two numbers: \"1,2\"-> 3', () => {
     expect(add("1,2")).toBe(3);
 });
 
-test('\"1,2\n3\"-> 6', () => {
-    expect(add("1,2\n3")).toBe(6);
+test('numbers with new line: \"1,2\n3\"-> 6', () => {
+    expect(add("1,2\n3,")).toBe(6);
 });
 
-test('\"1,2,3,4,5,6,7,12\"-> 40', () => {
+test('numbers with new line and null string: \"1,2\n3,,4\"-> 10', () => {
+    expect(add("1,2\n3,,4")).toBe(10);
+});
+
+test('3 digits numbers: \"1,2\n3,450\"-> 456', () => {
+    expect(add("1,2\n3,450")).toBe(456);
+});
+
+test('more than 2 numbers: \"1,2,3,4,5,6,7,12\"-> 40', () => {
     expect(add("1,2,3,4,5,6,7,12")).toBe(40);
 });
 
-test('\"1,2,3,4,5,6,7,12,1,2,3,4,5,6,7,12\"-> 79', () => {
+test('more than 2 numbers: \"1,2,3,4,5,6,7,12,1,2,3,4,4,6,7,12\"-> 79', () => {
     expect(add("1,2,3,4,5,6,7,12,1,2,3,4,4,6,7,12")).toBe(79);
 });
 
-test('\"1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\"-> 19', () => {
+test('way more than 2 numbers: \"1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\"-> 19', () => {
     expect(add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1")).toBe(19);
 });
 
-test('\"1,2,3,4\n5,6,7\n12\"-> 40', () => {
+test('new line and 2 digits numbers: \"1,2,3,4\n5,6,7\n12\"-> 40', () => {
     expect(add("1,2,3,4\n5,6,7\n12")).toBe(40);
 });
 
-test('\"1\n2\n3\n4\n5\n6\n7\n12\"-> 40', () => {
+test('no delimiter, just new line: \"1\n2\n3\n4\n5\n6\n7\n12\"-> 40', () => {
     expect(add("1\n2\n3\n4\n5\n6\n7\n12")).toBe(40);
+});
+
+test('custom delimiter, 2 numbers, no new lines: \"//;\n1;2\"-> 3', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
+
+test('custom delimiter with more than 1 char, no new lines: \"//ciao\n1ciao2\"-> 3', () => {
+    expect(add("//ciao\n1ciao2")).toBe(3);
+});
+
+test('custom delimiter, 2 numbers, new lines: \"//;\n1;2\n3\"-> 6', () => {
+    expect(add("//;\n1;2\n3")).toBe(6);
+});
+
+test('custom delimiter with more than 1 char, new lines: \"//ciao\n1ciao2\n3\"-> 6', () => {
+    expect(add("//ciao\n1ciao2\n3")).toBe(6);
+});
+
+test('\".\" as a delimiter: \"//.\n1.2\n3\n4.5\"-> 15', () => {
+    expect(add("//.\n1.2\n3\n4.5")).toBe(15);
+});
+
+test('\"?\" as a delimiter: \"//?\n1?2\n3\n4?5\"-> 15', () => {
+    expect(add("//?\n1?2\n3\n4?5")).toBe(15);
+});
+
+test('\"*\" as a delimiter: \"//*\n1*2\n3\n4*5\"-> 15', () => {
+    expect(add("//*\n1*2\n3\n4*5")).toBe(15);
+});
+
+test('\"****\" as a delimiter: \"//****\n1****2\n3\n4****5\"-> 15', () => {
+    expect(add("//*\n1*2\n3\n4*5")).toBe(15);
+});
+
+test('\"+\" as a delimiter: \"//+\n1+2\n3\n4+5\"-> 15', () => {
+    expect(add("//+\n1+2\n3\n4+5")).toBe(15);
+});
+
+
+test('\"+?\" as a delimiter: \"//+\n1+2\n3\n4+5\"-> 15', () => {
+    expect(add("//+?\n1+?2\n3\n4+?5")).toBe(15);
+});
+
+
+test('\"*?\" as a delimiter: \"//+?\n1+?2\n3\n4+?5\"-> 15', () => {
+    expect(add("//*?\n1*?2\n3\n4*?5")).toBe(15);
 });
